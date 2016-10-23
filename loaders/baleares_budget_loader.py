45e97c6dd7592ec71051b2f1f0d2d379ee2a9940
David Cabo <david.cabo@gmail.com>
2016-10-24 01:40:03 +0200

    Adapt budget structure after 2017 remapping civio/presupuesto-management#336

--- loaders/baleares_budget_loader.py
+++ loaders/baleares_budget_loader.py
@@ -9,6 +9,82 @@ import re
 
 class BalearesBudgetLoader(BudgetLoader):
 
+    def map_to_new_functional_structure(self, fc_code):
+        programme_mapping = {
+            '112B': '131A',
+            '112C': '131B',
+            '112E': '121C',
+            '112F': '151C',
+            '121E': '131B',
+            '121J': '761B',
+            '121K': '121H',
+            '124A': '912B',
+            '126A': '121B',
+            '126C': '151A',
+            '131A': '231A',
+            '131B': '231B',
+            '134A': '232A',
+            '141A': '211A',
+            '313A': '313D',
+            '315B': '324A',
+            '315C': '322C',
+            '315D': '121I',
+            '323D': '131C',
+            '441A': '562A',
+            '441B': '562B',
+            '443B': '571B',
+            '443C': '571A',
+            '443E': '441A',
+            '443K': '572A',
+            '443L': '571C',
+            '443M': '571B',
+            '443Q': '572A',
+            '457A': '461A',
+            '463B': '151C',
+            '463C': '151B',
+            '463D': '132A',
+            '463E': '151C',
+            '511C': '531A',
+            '511E': '522B',
+            '512A': '561A',
+            '513C': '521B',
+            '513D': '521A',
+            '514A': '531A',
+            '514B': '522A',
+            '521A': '551A',
+            '521B': '551B',
+            '533A': '571D',
+            '533B': '571E',
+            '533D': '571C',
+            '533F': '571F',
+            '533G': '571G',
+            '551A': '591A',
+            '611A': '141A',
+            '612A': '141D',
+            '612B': '141B',
+            '612C': '141C',
+            '612E': '141E',
+            '612G': '141F',
+            '612H': '141G',
+            '613A': '142A',
+            '613B': '142B',
+            '634A': '811A',
+            '731C': '731A',
+            '763A': '761A',
+        }
+
+        # We got the higher levels of the new functional classification from the client,
+        # so they're correct already.
+        if len(fc_code)<=4:
+            return fc_code
+        # But if we get a subprogramme code (either when loading the functional classification,
+        # or when loading data) we need to map the old subprogramme to the new codes.
+        else:
+            programme_id = fc_code[0:4]
+            subprogramme_extension_id = fc_code[4:6]
+            new_fc_code = programme_mapping.get(programme_id, programme_id) + subprogramme_extension_id
+            return new_fc_code
+
     # We don't have funding categories, so create a dummy one and assign everything to it
     def get_default_funding_categories(self):
         categories = BudgetLoader.get_default_funding_categories(self)
@@ -29,14 +105,15 @@ class BalearesBudgetLoader(BudgetLoader):
         return categories
 
     def add_functional_category(self, items, line):
+        fc_code = self.map_to_new_functional_structure(line[1])
         description = line[2]
 
         items.append({
-                'area': (line[1][0:1] if len(line[1])>=1 else None),
-                'policy': (line[1][0:2] if len(line[1])>=2 else None),
-                'function': (line[1][0:3] if len(line[1])>=3 else None),
-                'programme': (line[1][0:4] if len(line[1])>=4 else None),
-                'subprogramme': (line[1] if len(line[1])>=5 else None),
+                'area': (fc_code[0:1] if len(fc_code)>=1 else None),
+                'policy': (fc_code[0:2] if len(fc_code)>=2 else None),
+                'function': (fc_code[0:3] if len(fc_code)>=3 else None),
+                'programme': (fc_code[0:4] if len(fc_code)>=4 else None),
+                'subprogramme': (fc_code if len(fc_code)>=5 else None),
                 'description': description
             })
 
@@ -61,9 +138,10 @@ class BalearesBudgetLoader(BudgetLoader):
 
         # Parse functional code
         if is_expense:
-            # Functional codes: keep only the final part of the code
-            match = re.search('/([A-Z0-9]*)$', line[4])
-            fc_code = match.group(1)
+            # Functional codes: keep only the final part of the code...
+            fc_code = self._get_trailing_code(line[4])
+            # ...and map it to the new classification
+            fc_code = self.map_to_new_functional_structure(fc_code)
 
             fc_area = fc_code[0:1]
             fc_policy = fc_code[0:2]
@@ -105,8 +183,12 @@ class BalearesBudgetLoader(BudgetLoader):
             })
 
     def _get_trailing_code(self, full_code):
-        match = re.search('/([A-Z0-9]*)$', full_code)
-        return match.group(1)
+        # Check for complex code format, since sometimes (2017 so far) is not there
+        if full_code.find('/')==-1:
+            return full_code
+        else:
+            match = re.search('/([A-Z0-9]*)$', full_code)
+            return match.group(1)
 
     def _get_delimiter(self):
         return ','
