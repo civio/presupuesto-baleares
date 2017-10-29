0c059027e48f1e398a2a196b5f97c4c43dc51f6f
David Cabo <david.cabo@gmail.com>
2017-10-29 21:23:58 +0100

    Show 2018 as project in year selector civio/presupuesto-management#534

--- static/javascripts/theme.js
+++ static/javascripts/theme.js
@@ -80,13 +80,13 @@ $(document).ready(function(){
 
     $('.data-controllers .layout-slider .slider .slider-tick-label').each(function(){
       var val = $(this).html();
-      if (val === '2017'){
+      if (val === '2018'){
         $(this).html(val + '<br/><small><i> ('+ str2016[ $('html').attr('lang') ] +')</i></small>');
       }
     });
   };
 
-  // addYearSelectorCustomLabels();
+  addYearSelectorCustomLabels();
 
   if ($('body').hasClass('body-subprogrammes')) {
     addProjects();  
